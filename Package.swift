b51309e8648c28a73124e0a683794d47a8b50484
Jeffrey Macko <github.jm@macko.fr>
2022-10-25 14:12:29 +0200

    Add Platform

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
   name: "PackageGeneratorCLI",
+  platforms: [
+    .macOS(.v12),
+  ],
   products: [
     .executable(name: "package-generator-cli", targets: ["PackageGeneratorCLI"]),
   ],
