bcd8d0f1767d35d72dbe1f929a7b5b5f751b0f2c
Jeffrey Macko <mackoj@users.noreply.github.com>
2022-11-08 11:25:54 +0100

    Feat/intel (#11)
    Add support for mac intel in the builder script 
    fix #7

--- build.sh
+++ build.sh
@@ -1,16 +1,30 @@
 #!/usr/bin/env bash
 
+
+# prepare variables
 CLI="package-generator-cli"
 INFOJSONPATH="$CLI.artifactbundle/info.json"
-CLIBINPATH="$CLI.artifactbundle/arm64-apple-macosx/bin"
 TMPPATH=$(mktemp)
 TMPRELEASEFOLDER=$(mktemp -d)
-TMPRELEASEPROJECT=$TMPRELEASEFOLDER/PackageGeneratorCLI
-TAG=$(curl -s "https://api.github.com/repos/mackoj/PackageGeneratorCLI/tags" | jq --compact-output --raw-output '.[0].name ')
+REPO="mackoj/PackageGeneratorCLI"
 SOURCEFOLDER=$(pwd)
 
+if [[ $(uname -m) == "x86_64" ]]; then
+	TRIPLE="x86_64-apple-macosx"
+else
+	TRIPLE="arm64-apple-macosx"
+fi
+
+TMPRELEASEPROJECT=$TMPRELEASEFOLDER/PackageGeneratorCLI
+TAG=$(curl -s "https://api.github.com/repos/$REPO/tags" | jq --compact-output --raw-output '.[0].name ')
+CLIBINPATH="$CLI.artifactbundle/$TRIPLE/bin"
+ZIPOUTPUT=$CLI-$TRIPLE.artifactbundle.zip
+
+# generation
+############
+echo "Cloning $REPO @ $TAG"
 cd "$TMPRELEASEFOLDER" || exit
-git clone --depth 1 --branch "$TAG"  https://github.com/mackoj/PackageGeneratorCLI.git
+git clone --depth 1 --branch "$TAG"  https://github.com/$REPO.git
 cd "$TMPRELEASEPROJECT" || exit
 
 echo "Building $TAG"
@@ -23,17 +37,22 @@ echo "Creating Artifactbundle"
 mkdir -p $CLIBINPATH
 cp -f .build/release/$CLI $CLIBINPATH
 cp info.json "$INFOJSONPATH"
+
+echo "Updating Artifactbundle Info.json"
 jq '.artifacts."package-generator-cli".version = "'"$TAG"'"' "$INFOJSONPATH" > "$TMPPATH"
 mv "$TMPPATH" "$INFOJSONPATH"
 
-echo "Zip Artifactbundle"
-zip -r $CLI.artifactbundle.zip $CLI.artifactbundle -x ".*" -x "__MACOSX"
+jq '.artifacts."package-generator-cli".variants += [{ "path": "'"$TRIPLE"'/bin/package-generator-cli", "supportedTriples": ["'"$TRIPLE"'"] }] ' "$INFOJSONPATH" > "$TMPPATH"
+mv "$TMPPATH" "$INFOJSONPATH"
 
-echo "Compute Checksum"
-swift package compute-checksum $CLI.artifactbundle.zip
+echo "Zip Artifactbundle"
+zip -r $ZIPOUTPUT $CLI.artifactbundle -x ".*" -x "__MACOSX"
+cp $ZIPOUTPUT "$SOURCEFOLDER/$ZIPOUTPUT"
 
 echo "Cleaning"
 rm -rf $CLI.artifactbundle
-cp $CLI.artifactbundle.zip "$SOURCEFOLDER/$CLI.artifactbundle.zip"
 cd "$SOURCEFOLDER" || exit
 rm -rf "$TMPRELEASEFOLDER"
+
+echo "Compute Checksum"
+swift package compute-checksum $ZIPOUTPUT
