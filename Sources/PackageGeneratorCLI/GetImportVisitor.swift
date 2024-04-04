c70f946b4283a14c85dffc584a01d1e640e3b396
Jeffrey Macko <jeffrey.macko@decathlon.com>
2024-04-04 18:31:39 +0200

    Fix Swift syntax support

--- Sources/PackageGeneratorCLI/GetImportVisitor.swift
+++ Sources/PackageGeneratorCLI/GetImportVisitor.swift
@@ -4,21 +4,10 @@ import SwiftSyntax
 class GetImportVisitor: SyntaxRewriter {
   var imports: [String] = []
   
-  func visit(_ node: ImportPathComponentSyntax) -> Syntax {
+  override func visit(_ node: ImportPathComponentSyntax) -> ImportPathComponentSyntax {
     imports.append(node.name.text)
-    return node._syntaxNode
+    return node
   }
-//  override func visit(_ node: AccessPathSyntax) -> Syntax {
-//    let identifier = node.tokens.compactMap { ts -> String? in
-//      if case let .identifier(id) = ts.tokenKind, id.isEmpty == false {
-//        let trimmed = id.trimmingCharacters(in: .whitespacesAndNewlines)
-//        return trimmed.isEmpty ? nil : trimmed
-//      }
-//      return nil
-//    }
-//    imports.append(contentsOf: identifier)
-//    return node._syntaxNode
-//  }
   
   func drain() -> [String] {
     return imports
