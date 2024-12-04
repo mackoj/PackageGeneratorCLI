089f9e61664ef838d1e42edf589aae4831823bd0
Jeffrey Macko <jeffrey.macko@decathlon.com>
2024-12-04 17:24:06 +0100

    WIP

--- Sources/PackageGeneratorCLI/GetImportVisitor.swift
+++ Sources/PackageGeneratorCLI/GetImportVisitor.swift
@@ -2,14 +2,28 @@ import Foundation
 import SwiftSyntax
 
 class GetImportVisitor: SyntaxRewriter {
-  var imports: [String] = []
+  var imports: Set<String> = []
+  var importExclusion: Set<String> = []
   
+  override func visit(_ node: DeclReferenceExprSyntax) -> ExprSyntax {
+    if node.baseName.text.lowercased() == "canImport".lowercased() {
+      // Build this in a reccursive manner
+      //----------------------------------
+      // FunctionCallExprSyntax
+      // DeclReferenceExprSyntax
+      // identifier canimport
+      // LabeledExprListSyntax -> Toto
+    }
+    return ExprSyntax(node)
+  }
+
   override func visit(_ node: ImportPathComponentSyntax) -> ImportPathComponentSyntax {
-    imports.append(node.name.text.trimmingCharacters(in: .whitespacesAndNewlines))
+    imports.insert(node.name.text.trimmingCharacters(in: .whitespacesAndNewlines))
     return node
   }
   
   func drain() -> [String] {
-    return imports
+    imports.subtract(importExclusion)
+    return Array(imports)
   }
 }
